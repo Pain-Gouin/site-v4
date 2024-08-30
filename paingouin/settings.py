6d49d1c7f5cfbbd310f814595c3c469b8aa7cab8
Mathis Rimbert <mathis.rimbert@centrale.centralelille.fr>
2024-08-30 19:18:21 +0200

    collectstatic

--- paingouin/settings.py
+++ paingouin/settings.py
@@ -134,6 +134,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+
+COMPRESS_ROOT = 'paingouin/static/'
+
+COMPRESS_ENABLED = True
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
