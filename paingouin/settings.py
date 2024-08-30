0e1250ac4fb79925f390226566edc9855e96795e
Mathis Rimbert <mathis.rimbert@centrale.centralelille.fr>
2024-08-30 19:22:47 +0200

    debug off

--- paingouin/settings.py
+++ paingouin/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=5sf8@fhdxzr8(c!%-5xx1!5x6x07$%vc^0rr&$4ljgh&v5!w%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['paingouindev.rezoleo.fr', 'www.paingouindev.rezoleo.fr']
 
 
 # Application definition
